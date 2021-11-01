fc4c9694f8e82b7e46e9711be31a4b4e23f3c16d
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-11-01 13:36:56 -0700

    add more js to function from hw
    co-authored-by: Morgan Waites <morgan.waites@gmail.com>

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ obj.timeOfYear;
 obj.notes;
 Expected Output: see above;
 
+Describe: Place()
+
+Test: "It should show the object's properties that were first initialized."
+Code: let obj = Place("London", ["Regents Park", "Hampstead Heath", "Tate's Museum"], "October 2019", "My favorite city so far!");
+obj.location;
+obj.landmarks;
+obj.timeOfYear;
+obj.notes;
+Expected Output: see above;
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+function Places(){
+    this.places={};
+}
 function Place(location,landmarks, timeOfYear, notes){
     this.location = location;
     this.landmarks = landmarks;
@@ -5,67 +8,50 @@ function Place(location,landmarks, timeOfYear, notes){
     this.notes = notes;
 }
 
-
-let obj1 = new Place("London, England", ["Regents Park", "Hampstead Heath", "Tate's Museum"], "October 2019", "My favorite city so far!");
-
-let obj2 = new Place("Seattle, Washington", ["Pikes Place", "Space needle"], "July 2021", "One of my favorite close in places to visit!");
-
-let obj3 = new Place("Amsterdam, Netherlands", ["Canals", "Van Gogh Museum"], "October 2019", "A magical fun-filled city. Try the Dutch apple pie!");
-
-
 let newObjCounter=0;
 
-function displayPlaceTest(objName){
+function displayPlace(objName){
     newObjCounter +=1
     let newObj = "newObj"
     newObj = newObj+ newObjCounter;
-    $("#place").append("<a id='"+newObj+"' >" + objName.location + "</a>" + "</br>");
-    $("#"+newObj).append("<p class='"+newObj+"' >" + "Landmarks: " + objName.landmarks.join(", ") + "</p>");
-    $("#"+newObj).append("<p class='"+newObj+"' >"+ "Time visited: " + objName.timeOfYear + "</p>");
-    $("#"+newObj).append("<p class='"+newObj+"' >" + "Notes: " +objName.notes + "</p>");
+    $("#place").append("<a id="+newObj+">" + objName.location + "</a>" + "</br>");
+    $("#"+newObj).append("<p class="+newObj+">" + "Landmarks: " + objName.landmarks.join(", ") + "</p>");
+    $("#"+newObj).append("<p class="+newObj+">"+ "Time visited: " + objName.timeOfYear + "</p>");
+    $("#"+newObj).append("<p class="+newObj+">" + "Notes: " +objName.notes + "</p>");
     $("."+newObj).css("display","none");
     $("#"+newObj).click(function(){
         $("."+newObj).toggle();
     });
 };
 
-function displayPlaces(){
-    $("#place").append("<a id='obj1'>" + obj1.location + "</a>" + "</br>");
-    $("#obj1").append("<p class='prop'>" + "Landmarks: " + obj1.landmarks.join(", ") + "</p>");
-    $("#obj1").append("<p class='prop'>" + "Time visited: " + obj1.timeOfYear + "</p>");
-    $("#obj1").append("<p class='prop'>" + "Notes: " +obj1.notes + "</p>");
-    $("#obj1").click(function(){
-        $(".prop").toggle();
-    });
-    
-    $("#place").append("<a id='obj2'>" + obj2.location + "</a>" + "</br>");
-      $("#obj2").append("<p class='prop1'>" + "Landmarks: " + obj2.landmarks.join(", ") + "</p>");
-      $("#obj2").append("<p class='prop1'>" + "Time visited: " + obj2.timeOfYear + "</p>");
-      $("#obj2").append("<p class='prop1'>" + "Notes: " +obj2.notes + "</p>");
-    $("#obj2").click(function(){
-      $(".prop1").toggle();  
-  });
-    $("#place").append("<a id='obj3'>" + obj3.location + "</a>" +"</br>");
-      $("#obj3").append("<p class='prop2'>" + "Landmarks: " + obj3.landmarks.join(", ") + "</p>");
-      $("#obj3").append("<p class='prop2'>" + "Time visited: " + obj3.timeOfYear + "</p>");
-      $("#obj3").append("<p class='prop2'>" + "Notes: " +obj3.notes + "</p>");
-    $("#obj3").click(function(){
-      $(".prop2").toggle();
-  });
-}
+Places.prototype.addPlace(function(place){
+    this.places[place.location] = place;
+});
 
 $(document).ready(function(){
-    displayPlaces();
+    
+    let obj1 = new Place("London, England", ["Regents Park", "Hampstead Heath", "Tate's Museum"], "2019-10", "My favorite city so far!");
+
+    let obj2 = new Place("Seattle, Washington", ["Pikes Place", "Space needle"], "2021-07", "One of my favorite close in places to visit!");
+    
+    let obj3 = new Place("Amsterdam, Netherlands", ["Canals", "Van Gogh Museum"], "2019-10", "A magical fun-filled city. Try the Dutch apple pie!");
+    
+    displayPlace(obj1);
+    displayPlace(obj2);
+    displayPlace(obj3);
+
     $("#formOne").submit(function(event){
+
         event.preventDefault();
         const loc = $("#location").val();
         const landmarks = $("#landmarks").val();
         const arrayLandmarks = landmarks.split(", ");
         const time= $("#time").val();
         const notes= $("#notes").val();
+
         let obj4 = new Place(loc, arrayLandmarks, time, notes);
 
-        displayPlaceTest(obj4);
+        displayPlace(obj4);
 
     })
 });
